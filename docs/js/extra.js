fd85fb7b3678a520ad715be42f65128570c6db69
Rosa María Muñiz Garcia <mungarro@itacyl.es>
2024-05-21 13:03:44 +0200

    Enlace pagina formulio desde docs

---
+++ docs/js/extra.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var logoLink = document.querySelector('.md-logo');
+    if (logoLink) {
+        logoLink.href = 'http://www.formulio.es';
+    }
+});
